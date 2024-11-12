7d740f46eba9685b6b140442b7a0c22bb8e7cc66
Onurinal <onurinaldev@gmail.com>
2024-11-13 02:31:48 +0300

    + Added background and resize manager + Added basic loadscene script to manage scenes + Added basic animation for enemy walking

---
+++ Assets/BasicTowerDefender/Scripts/SpaceManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace TowerDefender.Level
+{
+    public class SpaceManager : MonoBehaviour
+    {
+        [SerializeField] private Transform backgroundModel;
+
+        [SerializeField] private Transform leftTree, rightTree;
+        [SerializeField] private Transform leftTreeModel, rightTreeModel;
+        private Camera mainCamera;
+
+        private void Start()
+        {
+            mainCamera = Camera.main;
+            if (mainCamera != null)
+            {
+                ReSizeBackgrounds();
+            }
+        }
+
+        private void ReSizeBackgrounds()
+        {
+            float aspectRatio = (float)mainCamera.pixelWidth / mainCamera.pixelHeight;
+
+            // Calculate the camera sizes in world units for 2d orthographic camera
+            float cameraHeight = mainCamera.orthographicSize * 2;
+            float cameraWidth = cameraHeight * aspectRatio;
+
+            // Calculate the size of background in "world units" based on its original scale
+            var spriteRenderer = backgroundModel.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                float backgroundOriginalScaleX = backgroundModel.localScale.x;
+                float backgroundOriginalScaleY = backgroundModel.localScale.y;
+
+                float backgroundWidth = spriteRenderer.bounds.size.x / backgroundOriginalScaleX;
+                float backgroundHeight = spriteRenderer.bounds.size.y / backgroundOriginalScaleY;
+
+                // Calculate the scaling factor for x and y based on camera size
+                float newScaleX = cameraWidth / backgroundWidth;
+                float newScaleY = cameraHeight / backgroundHeight;
+
+                backgroundModel.localScale = new Vector3(newScaleX, newScaleY, backgroundModel.localScale.z);
+            }
+
+            // Calculate only x scale factor because the game is designed for landscape orientation
+            var newLeftTreePositionX = -(cameraWidth / 2) + leftTreeModel.localScale.x / 1.2f;
+            var newRightTreePositionX = (cameraWidth / 2) - rightTreeModel.localScale.x / 1.2f;
+
+            leftTree.position = new Vector3(newLeftTreePositionX, leftTree.position.y, leftTree.position.z);
+            rightTree.position = new Vector3(newRightTreePositionX, rightTree.position.y, rightTree.position.z);
+        }
+    }
+}
